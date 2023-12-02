1bec7beb777a52cc749639d6b1621a83b67ca0e8
Ignacio07 <65515081+Ignacio07@users.noreply.github.com>
2023-12-02 02:08:08 -0300

    testing

--- src/app/Pago/page.tsx
+++ src/app/Pago/page.tsx
@@ -1,7 +1,31 @@
+
+import { WebpayPlus } from 'transbank-sdk'; // ES6
+
 export default function Pago(){
+    const WebpayPlus = require('transbank-sdk').WebpayPlus; // ES5
+
+
+async function createTransaction(buyOrder, sessionId, amount, returnUrl) {
+    try {
+      const createResponse = await (new WebpayPlus.Transaction()).create(
+        buyOrder, 
+        sessionId, 
+        amount, 
+        returnUrl
+      );
+  
+      
+  
+      return createResponse;
+    } catch (error) {
+      console.error('Error al crear la transacción:', error);
+      throw error;
+    }
+  }
     return(
-        <div>
-            <h1>Pago</h1>
-        </div>
+    <form action="https://webpay3gint.transbank.cl/webpayserver/initTransaction" method="POST">
+        <input type="hidden" name="token_ws" value="01ab97bd2065ec3543290bdf5ecbba6026948536e017e97f5a6ccfcc5a5d68bc"/>
+        <input type="submit" value="Pagar"/>
+    </form>
     )
 }
