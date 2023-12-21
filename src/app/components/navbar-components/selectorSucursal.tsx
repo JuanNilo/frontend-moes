ebd01d46984d69438834144bd43938bd11038fba
JuanNilo <juan.nilo@alumnos.ucn.cl>
2023-12-21 02:27:01 -0300

    UPDATE: Selector sucursal

--- src/app/components/navbar-components/selectorSucursal.tsx
+++ src/app/components/navbar-components/selectorSucursal.tsx
@@ -3,28 +3,49 @@ import { Colors } from "@/app/extras/styles";
 import { sucursales } from "@/app/components/data/sucursales.json";
 import { useSucursalContext } from "@/app/components/context/SucursalContext";
 const {primary, secondary, tertiary} = Colors;
-import React, {useContext} from "react";
+import React, {useContext, useEffect} from "react";
+import { useQuery, gql } from "@apollo/client";
+import client from "@/app/components/client";
 
-const listaSucursales = sucursales
+
+interface Sucursal {
+    _id: string;
+    name: string;
+    address: string;
+}
 
 export default function SelectoSucursal(){
-
+    
     const {sucursalSeleccionada, seleccionarSucursal} = useSucursalContext();
 
-    const handleSeleccionarSucursal = (event) => {
+    const [listaSucursales, setListaSucursales] = React.useState<Sucursal[]>([] as Sucursal[]);
+
+    const handleSeleccionarSucursal =  (event) => {
         const nuevaSucursal = event.target.value;
-        console.log(nuevaSucursal)
         seleccionarSucursal(nuevaSucursal);
       };
+      useEffect(() => {
+       client.query({
+            query: gql`
+            query{
+                FIND_STORES{
+                    _id
+                    name
+                    address
+                }
+            }`
+            ,
+        }).then((result) => setListaSucursales(result.data.FIND_STORES));
+    }, []);
 
     return(
         <div className="w-[35%] md:w-[30%]">
             <select id="sucursales" style={{backgroundColor: tertiary}} className=" border text-gray-900 text-md font-bold rounded-full block p-2 w-[100%] md:w-[80%] items-center mx-2 md:mx-10 h-[100%] " value={sucursalSeleccionada || ''} onChange={handleSeleccionarSucursal}>
                 <option>Sucursal</option>
                 {
-                    listaSucursales.map((sucursal) => (
-                        <option key={sucursal.id} value={sucursal.id}>
-                            {sucursal.nombre}
+                    listaSucursales.map((sucursal : Sucursal) => (
+                        <option key={sucursal._id} value={sucursal._id}>
+                            {sucursal.name}
                         </option>
                     ))
                 }
